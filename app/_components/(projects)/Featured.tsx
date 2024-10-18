27f27dc337de02ab2131f0bece968ed015a58406
Joshua Rashtian <joshrashtian1@gmail.com>
2024-10-17 17:38:05 -0700

    redid featured
    (cherry picked from commit 8f35fee53c5465c3c0e600b8d66987196ed62b7f)

--- app/_components/(projects)/Featured.tsx
+++ app/_components/(projects)/Featured.tsx
@@ -1,52 +1,34 @@
 import React from "react";
 import FeaturedCard from "./FeaturedCard";
 import Image from "next/image";
-import { IoArchive, IoNavigate } from "react-icons/io5";
-import Link from "next/link";
 
 const FeaturedProjects = () => {
   return (
-    <section className="p-4">
-      <IoArchive className="ml-4 text-5xl" />
-      <h2 className="ml-4 text-2xl font-semibold">Case Studies</h2>
-
-      <ul className="grid w-full grid-cols-1 gap-3 scroll-smooth whitespace-nowrap bg-zinc-100/30 p-3 xl:grid-cols-3 dark:bg-black">
+    <>
+      <h2 className="ml-24 text-2xl font-semibold">Featured Projects</h2>
+      <ul className="snap-point no-scrollbar flex h-96 w-full snap-x snap-mandatory flex-row gap-24 overflow-x-scroll scroll-smooth whitespace-nowrap bg-zinc-100/30 p-10 px-52">
         <FeaturedCard
           href="/projects/mesaconnect"
-          className="group flex flex-col justify-between border-2 bg-gradient-to-br from-orange-50 to-zinc-100 dark:from-orange-700 dark:to-zinc-800"
+          className="flex flex-col justify-between"
         >
-          <ul className="relative h-72 w-full rounded-lg">
-            <Link
-              href={"https://mesaconnect.io"}
-              className="absolute bottom-2 right-2 z-50 translate-y-3 text-3xl text-white opacity-0 drop-shadow-2xl duration-300 hover:text-blue-400 group-hover:translate-y-0 group-hover:opacity-100"
-            >
-              <IoNavigate />
-            </Link>
-            <ul className="absolute z-40 h-full w-full bg-gradient-to-t from-black/40 via-transparent to-zinc-500/40" />
+          <ul className="relative h-48 w-full rounded-lg">
             <Image
-              src={require("../../(assets)/images/mesaconnectbanner.png")}
+              src={require("../../(assets)/projects/MESAPicture.png")}
               fill
               objectFit="cover"
-              className="z-30"
+              className="rounded-lg"
               alt="mesa"
             />
           </ul>
-          <ul className="flex flex-row items-center">
-            <Image
-              src={require("@/app/(assets)/images/icons/mesalogo.png")}
-              alt="logo"
-              width={76}
-              height={56}
-            />
-            <ul className="lg:p-3">
-              <FeaturedCard.Title>
-                Connecting To Your Peers With MESAConnect
-              </FeaturedCard.Title>
-            </ul>
+          <ul>
+            <FeaturedCard.Title>
+              Power Up Your STEM Learning With MESAConnect
+            </FeaturedCard.Title>
+            <FeaturedCard.Desc>Join Today!</FeaturedCard.Desc>
           </ul>
         </FeaturedCard>
       </ul>
-    </section>
+    </>
   );
 };
 
